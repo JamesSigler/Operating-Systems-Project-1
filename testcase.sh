15fe6e6aa230a2af7743078d0a34cf5193722c18
JamesSigler <55518432+JamesSigler@users.noreply.github.com>
2019-10-17 07:43:27 -0500

    Add files via upload

---
+++ testcase.sh
@@ -0,0 +1,22 @@
+
+if [ -f  project1_2-1 ]
+then
+
+	echo -e "2 children\n"
+	./project1_2-1 2
+	echo -e "4 Children\n"
+	./project1_2-1 4
+	echo -e "8 Children\n
+	./project1_2-1 8	
+fi
+
+if [ -f  project1_2-5 ]
+then
+	echo -e "2 children\n"
+	./project1_2-5 2
+	echo -e "4 Children\n"
+	./project1_2-5 4
+	echo -e "8 Children\n
+	./project1_2-5 8		
+fi
+
