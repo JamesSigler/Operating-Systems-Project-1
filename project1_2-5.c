b0158a12b580710fb2dadc4a3e2324176a570431
JamesSigler <55518432+JamesSigler@users.noreply.github.com>
2019-10-17 07:25:45 -0500

    Add files via upload

--- project1_2-5.c
+++ project1_2-5.c
@@ -1,74 +1,39 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
 
-int count = 0;
+#include <stdlib.h>
 
 int main(int argc, int** argv)
 {
 	
 	//calling parent argv number of times
-	parent(argv[1]);
-	
-	
-}
-
-//child process
-void child(int input)
-{
-	if(count != input)
-	{
-		//child pid
-		pid_t cpid; 
-		
-		//forking the child 
-		cpid = fork();
-		
-		//if running another child wait
-		if(cpid > 0)
-		{
-			wait(0);
-		}
-		
-		//checking for fork failure and printing error
-		if(cpid < 0)
-		{
-			printf("Error: Child creation failed");
-		}
-		
-		//if fork succeeds print the pid
-		else if(cpid == 0)
-		{
-			printf("\nHello from pid %d", getpid());
-			exit(0);
-			count++;
-		}
-	}
-	else
-	{
-		exit(0);
-	}
+	repPID(argv[1]);
 }
 
 //parent process
-void parent(int input)
+void repPID(int input)
 {
-	//parent pid
-	pid_t ppid;
-	
-	//forking pid
-	ppid = fork();
-		
-	//fail print error
-	if(ppid < 0)
+    
+	for(int i = 0; i < input; i++)
 	{
-		printf("Error: Parent creation failed");
-	}
-		
-	//succeed, call child process
-	else if(ppid == 0)
-	{
-		child(input);
+    	//forking pid
+        pid_t pid = fork();
+    		
+    	//fail print error
+    	if(pid < 0)
+    	{
+    		printf("Error: pid creation failed");
+    	}
+    	else if(pid > 0)
+    	{
+    	    wait(0);
+    	    exit(0);
+    	}
+    	//succeed, call child process
+    	else if(pid == 0)
+    	{
+    		printf("Hello from pid %d\n", getpid());
+    	}
 	}
 }
